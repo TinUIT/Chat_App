86c829ed56bfed7946fd8190624295576395f592
TinUIT <20522017@gm.uit.edu.vn>
2023-01-06 11:21:07 +0700

    addfirebase

--- firebase.js
+++ firebase.js
@@ -5,7 +5,13 @@ import "firebase/compat/firestore";
 import { initializeApp } from "firebase/compat/app";
 
 const firebaseConfig = {
-  
+        apiKey: "AIzaSyD7APluopQvzQfTJ2DZjNF1wfCEdFsffIg",
+        authDomain: "chat-app-9bbb0.firebaseapp.com",
+        projectId: "chat-app-9bbb0",
+        storageBucket: "chat-app-9bbb0.appspot.com",
+        messagingSenderId: "985548449156",
+        appId: "1:985548449156:web:90f6b187f5b0913ff5a9d1",
+        measurementId: "G-D7FG7R0ZME",
 };
 
 let app;
